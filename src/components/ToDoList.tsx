fee56aef1e249937356db658a0232a2d080124be
DaniilT048 <tis.daniil98@gmail.com>
2025-04-06 14:47:12 +0300

    added input and button

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 
 function ToDoList(){
-    const [text, setText] = useState('Hard works')
-    console.log(setText)
+    // const [tasks, setTasks] = useState([])
+    const [newTask, setNewTask] = useState('')
+
+    function handleNewTask(event: any):void {
+        setNewTask(event.target.value)
+    }
 
     return (
-        <div>
-            <h1>To do list</h1>
-            <input/>
+            <div>
+                <h1>To do list</h1>
+                <div>
+                    <input
+                    type="text"
+                    placeholder="Add task.."
+                    value={newTask}
+                    onChange={handleNewTask}
+                    />
+                <button>add</button>
+                <button>delete</button>
+            </div>
             <ol>
-                {text}
+
             </ol>
         </div>
     )
